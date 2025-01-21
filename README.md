fe4c36f4f4f7c3775511f077008705e2f63940b8
cliebhardt <liebhadt.christian@gmail.com>
2025-01-21 10:26:52 +0100

    button-icon-issues

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Contains examples of issues encountered while using webforj to showcase for debugging.
+When setting an `Icon` on a `Button` with `setIcon(Component)`, according to the documentation, the text should be 
+cleared and the expanse should be set to `Expanse.NONE`. However the text is not cleared and remains in place, the icon
+is simply append to it.
--- src/main/java/com/webforj/issues/views/HelloWorldView.java
+++ src/main/java/com/webforj/issues/views/HelloWorldView.java
@@ -1,13 +1,13 @@
 package com.webforj.issues.views;
 
 import com.webforj.component.Composite;
-import com.webforj.component.Theme;
+import com.webforj.component.Expanse;
 import com.webforj.component.button.Button;
-import com.webforj.component.button.ButtonTheme;
-import com.webforj.component.field.TextField;
+import com.webforj.component.html.elements.Paragraph;
+import com.webforj.component.icons.Icon;
+import com.webforj.component.icons.TablerIcon;
 import com.webforj.component.layout.flexlayout.FlexDirection;
 import com.webforj.component.layout.flexlayout.FlexLayout;
-import com.webforj.component.toast.Toast;
 import com.webforj.router.annotation.Route;
 
 /**
@@ -17,17 +17,18 @@ import com.webforj.router.annotation.Route;
 public class HelloWorldView extends Composite<FlexLayout> {
 
   private FlexLayout self = getBoundComponent();
-  private TextField hello = new TextField("What is your name?");
-  private Button btn = new Button("Say Hello");
 
   public HelloWorldView(){
     self.setDirection(FlexDirection.COLUMN);
     self.setMaxWidth(300);
     self.setStyle("margin", "1em auto");
 
-    btn.setTheme(ButtonTheme.PRIMARY)
-        .addClickListener(e -> Toast.show("Welcome to webforJ Starter " + hello.getValue() + "!", Theme.GRAY));
+    Icon icon = TablerIcon.create("heart");
+    Button btn = new Button();
+    btn.setText("Should not appear");
+    btn.setIcon(icon);
 
-    self.add(hello, btn);
+
+    self.add(btn);
   }
 }
